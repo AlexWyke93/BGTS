75c357e7c3eae7510e5ab12fd4a03973d711e18f
AlexWyke93 <alexwyke93@gmail.com>
2018-10-13 08:38:22 +0100

    Added db_quote() to improve security.

--- create_events_table.php
+++ create_events_table.php
@@ -23,7 +23,7 @@
     $result = mysqli_query($connection, $newTable);
 
     if (!$result) {
-        die("Error in Code: " . mysqli_error($connection));
+        die(db_quote("Error in Code: " . mysqli_error($connection)));
     }
     else
     {
