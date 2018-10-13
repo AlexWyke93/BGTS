91daaf56276c922700a5df681e15e9562ff4e8fd
AlexWyke93 <alexwyke93@gmail.com>
2018-10-13 12:15:05 +0100

    test.php - reduced line lengths & fixed if/else

--- test.php
+++ test.php
@@ -8,18 +8,19 @@
 
     require "db_functions.php";
 
-    $result = db_query("INSERT INTO `artists`(
-    `artistId`, `artistName`, `artistBio`, `imageFN`) VALUES (
-    NULL,'Bob Wyke','I am a junior tattoo artist.','test')"
-    );
-    $result = db_query("INSERT INTO `artists`
-    (`artistId`, `artistName`, `artistBio`, `imageFN`) VALUES (
-    NULL,'Spike','I am a senior tattoo artist.','test')"
-    );
+    $result = db_query(
+        "INSERT INTO `artists`(
+        `artistId`, `artistName`, `artistBio`, `imageFN`) VALUES (
+        NULL,'Bob Wyke','I am a junior tattoo artist.','test')"
+        );
+    $result = db_query(
+        "INSERT INTO `artists`(
+        `artistId`, `artistName`, `artistBio`, `imageFN`) VALUES (
+        NULL,'Spike','I am a senior tattoo artist.','test')"
+        );
 
-    if ($result === false){
+    if ($result === false) {
         $error = db_error();
-    } else
-    {
+    } else {
         echo "Inserted Data!!!!";
     }
