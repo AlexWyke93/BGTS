3b6990ebd7d48d69c804095014fa4c0aa2b609e0
AlexWyke93 <alexwyke93@gmail.com>
2018-10-13 12:17:14 +0100

    test.php - reduced line lengths & fixed if/else

--- test.php
+++ test.php
@@ -12,12 +12,12 @@
         "INSERT INTO `artists`(
         `artistId`, `artistName`, `artistBio`, `imageFN`) VALUES (
         NULL,'Bob Wyke','I am a junior tattoo artist.','test')"
-        );
+    );
     $result = db_query(
         "INSERT INTO `artists`(
         `artistId`, `artistName`, `artistBio`, `imageFN`) VALUES (
         NULL,'Spike','I am a senior tattoo artist.','test')"
-        );
+    );
 
     if ($result === false) {
         $error = db_error();
