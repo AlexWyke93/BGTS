8eb0b1f020a7e4cb653bd58a1f07dedb1c742039
AlexWyke93 <alexwyke93@gmail.com>
2018-10-13 12:07:05 +0100

    test.php - reduced line lengths

--- test.php
+++ test.php
@@ -8,10 +8,12 @@
 
     require "db_functions.php";
 
-    $result = db_query("INSERT INTO `artists`(`artistId`, `artistName`, `artistBio`, `imageFN`) VALUES 
-                              (NULL,'Bob Wyke','I am a junior tattoo artist.','test')");
-    $result = db_query("INSERT INTO `artists`(`artistId`, `artistName`, `artistBio`, `imageFN`) VALUES 
-                              (NULL,'Spike','I am a senior tattoo artist.','test')");
+    $result = db_query("INSERT INTO `artists`(`artistId`, `artistName`, `artistBio`, `imageFN`) VALUES (
+                                NULL,'Bob Wyke','I am a junior tattoo artist.','test')"
+                                );
+    $result = db_query("INSERT INTO `artists`(`artistId`, `artistName`, `artistBio`, `imageFN`) VALUES (
+                                 NULL,'Spike','I am a senior tattoo artist.','test')"
+                                );
 
     if ($result === false){
         $error = db_error();
