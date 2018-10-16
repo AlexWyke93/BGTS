83da8293138065f5386b2e1dff933d7cfae1c6f7
AlexWyke93 <alexwyke93@gmail.com>
2018-10-16 16:43:57 +0100

    added artists.php file

---
+++ artists.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<?php
+/**
+ * PHP version 7.2.10
+ *
+ * @category PHP
+ * @package  BGTS-BE
+ * @author   Alexander Wyke <alexwyke93@gmail.com>
+ * @license  none
+ * @link     n/a
+ */
+?>
+</body>
+</html>
