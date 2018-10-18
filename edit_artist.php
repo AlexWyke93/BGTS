5cd75a4cbad919a564460a86bc16f67f9e11fc38
AlexWyke93 <alexwyke93@gmail.com>
2018-10-18 01:06:01 +0100

    edit_artist.php added

--- edit_artist.php
+++ edit_artist.php
@@ -1,7 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: minim
- * Date: 17/10/2018
- * Time: 20:04
- */
+include 'db_functions.php';
+
+$id = $_POST['artistId'];
+$artistName = htmlentities($_POST['artistName']);
+$artistBio = htmlentities($_POST['artistBio']);
+$image = htmlentities($_POST['imageFN']);
+
+try
+{
+    $result = Db_query(
+        "UPDATE artists SET artistName='" . $artistName . "', artistBio='" . $artistBio . "', imageFN='" . $image . "', WHERE artistId=" . $id . ";"
+    );
+
+    echo "Artist: " . $artistName . " has been edited!!";
+}
+catch (Exception $e)
+{
+    echo "Exception has occurred: " . $e->getMessage();
+}
+?>
+</body>
+</html>
