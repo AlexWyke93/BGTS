ad21c45e9ec9b3aef57f9e31f3ed77d35dd01de5
AlexWyke93 <alexwyke93@gmail.com>
2018-11-06 23:23:40 +0000

    created add_event.php

---
+++ add_event.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <title>Add Event</title>
+</head>
+<body>
+<?php
+include 'db_functions.php';
+
+$eventName = htmlentities($_POST['eventName']);
+$eventDate = htmlentities($_POST['eventDate']);
+$eventDesc = htmlentities($_POST['eventDesc']);
+
+include 'upload.php';
+
+$path = "Uploads/" . $_FILES['uploaded_file']['name'];
+
+$result = Db_query("INSERT INTO events VALUES (
+NULL, '$eventName', '$eventDate', '$eventDesc', NULL, '$path'
+)"
+);
+
+if (!$result) {
+    die($error = Db_error());
+} else {
+    echo "<h3>Record Added!!!!</h3>";
+}
+?>
+</body>
+</html>
