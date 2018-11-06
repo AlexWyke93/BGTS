9528b0f422b37533ddd4968ad867fcc77fe37d97
AlexWyke93 <alexwyke93@gmail.com>
2018-11-06 23:33:48 +0000

    created events.php

---
+++ events.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <title>Artists</title>
+</head>
+<body>
+<?php
+/**
+ * PHP version 7.2.10
+ *
+ * @category PHP
+ * @package  BGTS-BE
+ * @author   Alexander Wyke <alexwyke93@gmail.com>
+ * @license  none
+ * @link     n/a
+ */
+
+include 'db_functions.php';
+
+$connection = Db_connect();
+
+$result = Db_query("SELECT * FROM events");
+
+
+if ($result === false) {
+    return false;
+}
+
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<h1>" . $row['eventName'] . "</h1>" .
+        "<h1>" . $row['eventDate'] . "</h1>" .
+        "<h3>" . $row['eventDesc'] . "</h3>" .
+        "<p>" . $row['addedDate'] . "</p>" .
+        "<img src='" . $row['imageFN'] . "' alt='" . $row['eventName'] . "/>";
+}
+
+?>
+</body>
+</html>
