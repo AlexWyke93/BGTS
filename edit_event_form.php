86b294602b9e9e6ab9ca1d2de0cbe486550b9150
AlexWyke93 <alexwyke93@gmail.com>
2018-11-07 00:01:09 +0000

    created edit_event_form.php

---
+++ edit_event_form.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <title>Edit Artist Form</title>
+</head>
+<body>
+<?php
+include 'db_functions.php';
+
+$id = $_GET['eventId'];
+
+if ($id != NULL || $id != 0) {
+    try
+    {
+        $result = Db_query(
+            "SELECT * FROM events WHERE eventId=" . $id . ";"
+        );
+
+        $row = mysqli_fetch_row($result);
+
+        $id = $row[0];
+        $eventName = $row[1];
+        $eventDate = $row[2];
+        $eventDescription = $row[3];
+        $path = $row[5];
+    }
+    catch (Exception $e)
+    {
+        echo "Exception Occurred: " . $e->getMessage();
+    }
+}
+?>
+
+<form role="form" enctype="multipart/form-data" name="edit" method="POST" action="edit_artist.php">
+    <input type="hidden" value="<?php echo $id; ?>" name="id" />
+
+    <label for="artistName">Event Name: </label>
+    <input name="artistName" type="text" value="<?php echo $eventName; ?>">
+
+    <label for="eventDate">Event Date: </label>
+    <input type="date" name="eventDate"
+           value="<?php echo $eventDate; ?>"
+           min="2018-01-01">
+
+    <label for="artistBio">Event Description: </label>
+    <textarea name="artistBio"><?php echo $eventDescription; ?></textarea>
+
+    <label for="uploaded_file">Artist Avatar: </label>
+    <input type="file" name="uploaded_file" value="<?php echo $path; ?>">
+    <!-- The max file size is within a hidden field.
+    This is set to 2MB and will be handed later within
+    the PHP script. -->
+    <input type="hidden" name="MAX_FILE_SIZE" value="2097152" />
+
+    <input type="submit" name="submit" value="Edit Artist">
+</form>
+</body>
+</html>
