0b130e8531d2afb81f3faf6a6f36c04edd6ec750
AlexWyke93 <alexwyke93@gmail.com>
2019-01-12 17:28:52 +0000

    new

---
+++ add_tattoo_form.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * PHP version 7.2
+ *
+ * @category PHP
+ * @package  BGTS-BE
+ * @author   Alexander Wyke <alexwyke93@gmail.com>
+ * @license  none
+ * @link     n/a
+ */
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <title>Add Tattoo Form</title>
+</head>
+<body>
+    <h1>New Tattoo</h1>
+    <form id="add" role="form" method="POST" action="add_tattoo.php" enctype="multipart/form-data">
+        <label for="tattooName">Tattoo Name: </label>
+        <input type="text" name="tattooName">
+
+        <?php
+        include 'db_functions.php';
+
+        $result = Db_query("SELECT artistId, artistName FROM artists ORDER BY artistId ASC");
+
+        echo "<label for='artistSelection'>Select Artist: </label>";
+        echo "<select name='artistSelection' value='Artist'>";
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<option value=$row[artistId]>$row[artistName]</option>";
+        }
+        echo "</select>";
+
+        ?>
+
+        <label for="tattooDesc">Tattoo Description: </label>
+        <textarea type="text" name="tattooDesc"></textarea>
+
+        <label for="tattooType">Tattoo Type: </label>
+        
+
+        <label for="uploaded_file">Tattoo Image: </label>
+        <input type="file" name="uploaded_file">
+        <!-- The max file size is within a hidden field.
+        This is set to 2MB and will be handed later within
+        the PHP script. -->
+        <input type="hidden" name="MAX_FILE_SIZE" value="2097152" />
+
+        <input type="submit" name="submit" value="Add Tattoo">
+    </form>
+</body>
+</html>
---
+++ create_tattootype_table.php
@@ -0,0 +1,29 @@
+/**
+ * PHP version 7.2
+ *
+ * @category PHP
+ * @package  BGTS-BE
+ * @author   Alexander Wyke <alexwyke93@gmail.com>
+ * @license  none
+ * @link     n/a
+ */
+
+    require "Db_functions.php";
+
+    $connection = Db_connect();
+
+    $result = Db_query(
+        "CREATE TABLE tattootype (
+          tattootypeId INT AUTO_INCREMENT PRIMARY KEY NOT NULL UNIQUE,
+          tattootype VARCHAR(150) NOT NULL,
+          tattootypeDescription TEXT NOT NULL,
+        );"
+    );
+
+    $result = mysqli_query($connection, $newTable);
+
+if (!$result) {
+    die(htmlentities(Db_quote("Error in Code: " . mysqli_error($connection))));
+} else {
+    echo "Tattoo Type Table Created Successfully!!";
+}
